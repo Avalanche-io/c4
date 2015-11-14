16feffb3c1b08256db8ee5b5e2e86bc44c18d399
Joshua Kolden <joshua@crackcreative.com>
2015-11-13 21:06:08 -0800

    Fixed mutability bug, id.String() was changing id

--- id.go
+++ id.go
@@ -1,41 +1,41 @@
 package c4
 
 import (
-	"bytes"
-	"crypto/sha512"
-	"hash"
-	"math/big"
-	"strconv"
+  "bytes"
+  "crypto/sha512"
+  "hash"
+  "math/big"
+  "strconv"
 )
 
 // using the flickr character set which removes:
 // ['=', '+', '_', '0', 'O', 'I', 'l'] from base64
 // to reduce transcription errors, and make friendlier URLs
 const (
-	charset = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ"
-	base    = 58
+  charset = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ"
+  base    = 58
 )
 
 var (
-	lut     [256]byte
-	lowbyte = []byte("1")
-	prefix  = []byte{'c', '4'}
-	idlen   = 90
+  lut     [256]byte
+  lowbyte = []byte("1")
+  prefix  = []byte{'c', '4'}
+  idlen   = 90
 )
 
 func init() {
-	for i := 0; i < len(lut); i++ {
-		lut[i] = 0xFF
-	}
-	for i := 0; i < len(charset); i++ {
-		lut[charset[i]] = byte(i)
-	}
+  for i := 0; i < len(lut); i++ {
+    lut[i] = 0xFF
+  }
+  for i := 0; i < len(charset); i++ {
+    lut[charset[i]] = byte(i)
+  }
 }
 
 type errBadChar int
 
 func (e errBadChar) Error() string {
-	return "non c4 character at position " + strconv.Itoa(int(e))
+  return "non c4 character at position " + strconv.Itoa(int(e))
 }
 
 // ID represents a C4 Asset ID.
@@ -43,69 +43,71 @@ type ID big.Int
 
 // ParseID parses a C4 ID string into an ID.
 func ParseID(src string) (*ID, error) {
-	return ParseBytesID([]byte(src))
+  return ParseBytesID([]byte(src))
 }
 
 // ParseBytesID parses a C4 ID as []byte into an ID.
 func ParseBytesID(src []byte) (*ID, error) {
-	bigNum := new(big.Int)
-	bigBase := big.NewInt(base)
-	for i := 2; i < len(src); i++ {
-		b := lut[src[i]]
-		if b == 0xFF {
-			return nil, errBadChar(i)
-		}
-		bigNum.Mul(bigNum, bigBase)
-		bigNum.Add(bigNum, big.NewInt(int64(b)))
-	}
-	id := ID(*bigNum)
-	return &id, nil
+  bigNum := new(big.Int)
+  bigBase := big.NewInt(base)
+  for i := 2; i < len(src); i++ {
+    b := lut[src[i]]
+    if b == 0xFF {
+      return nil, errBadChar(i)
+    }
+    bigNum.Mul(bigNum, bigBase)
+    bigNum.Add(bigNum, big.NewInt(int64(b)))
+  }
+  id := ID(*bigNum)
+  return &id, nil
 }
 
 func (id *ID) String() string {
-	return string(id.Bytes())
+  return string(id.Bytes())
 }
 
 // Bytes encodes the written bytes to C4 ID format.
 func (id *ID) Bytes() []byte {
-	bigNum := big.Int(*id)
-	bigBase := big.NewInt(base)
-	bigZero := big.NewInt(0)
-	bigMod := new(big.Int)
-	var encoded []byte
-	for bigNum.Cmp(bigZero) > 0 {
-		bigNum.DivMod(&bigNum, bigBase, bigMod)
-		encoded = append([]byte{charset[bigMod.Int64()]}, encoded...)
-	}
-	// padding
-	diff := idlen - 2 - len(encoded)
-	encoded = append(bytes.Repeat(lowbyte, diff), encoded...)
-	// c4... prefix
-	encoded = append(prefix, encoded...)
-	return encoded
+  var bigNum big.Int
+  bigID := big.Int(*id)
+  bigNum.Set(&bigID)
+  bigBase := big.NewInt(base)
+  bigZero := big.NewInt(0)
+  bigMod := new(big.Int)
+  var encoded []byte
+  for bigNum.Cmp(bigZero) > 0 {
+    bigNum.DivMod(&bigNum, bigBase, bigMod)
+    encoded = append([]byte{charset[bigMod.Int64()]}, encoded...)
+  }
+  // padding
+  diff := idlen - 2 - len(encoded)
+  encoded = append(bytes.Repeat(lowbyte, diff), encoded...)
+  // c4... prefix
+  encoded = append(prefix, encoded...)
+  return encoded
 }
 
 // IDEncoder generates C4 Asset IDs.
 type IDEncoder struct {
-	h hash.Hash
+  h hash.Hash
 }
 
 // NewIDEncoder makes a new IDEncoder.
 func NewIDEncoder() *IDEncoder {
-	return &IDEncoder{
-		h: sha512.New(),
-	}
+  return &IDEncoder{
+    h: sha512.New(),
+  }
 }
 
 // Write writes bytes to the hash that makes up the ID.
 func (e *IDEncoder) Write(b []byte) (int, error) {
-	return e.h.Write(b)
+  return e.h.Write(b)
 }
 
 // ID gets the ID for the written bytes.
 func (e *IDEncoder) ID() *ID {
-	b := new(big.Int)
-	b.SetBytes(e.h.Sum(nil))
-	id := ID(*b)
-	return &id
+  b := new(big.Int)
+  b.SetBytes(e.h.Sum(nil))
+  id := ID(*b)
+  return &id
 }
--- id_test.go
+++ id_test.go
@@ -1,87 +1,86 @@
 package c4_test
 
 import (
-	"fmt"
-	"io"
-	// "math"
-	"math/big"
-	"strings"
-	"testing"
+  "fmt"
+  "io"
+  "math/big"
+  "strings"
+  "testing"
 
-	"github.com/cheekybits/is"
-	"github.com/etcenter/c4go"
+  "github.com/cheekybits/is"
+  "github.com/etcenter/c4go"
 )
 
 var _ io.Writer = (*c4.IDEncoder)(nil)
 var _ fmt.Stringer = (*c4.ID)(nil)
 
 func encode(src io.Reader) *c4.ID {
-	e := c4.NewIDEncoder()
-	_, err := io.Copy(e, src)
-	if err != nil {
-		panic(err)
-	}
-	return e.ID()
+  e := c4.NewIDEncoder()
+  _, err := io.Copy(e, src)
+  if err != nil {
+    panic(err)
+  }
+  return e.ID()
 }
 
 func TestEncoding(t *testing.T) {
-	is := is.New(t)
+  is := is.New(t)
 
-	for _, test := range []struct {
-		In  io.Reader
-		Exp string
-	}{
-		{
-			In:  strings.NewReader(``),
-			Exp: "c459CSJESBh38BxDwwxNFKTXE4cC9HASGe3bhtN6z58GbwLqpCyRaKyZSvBAvTdF5NpSTPdUMH4hHRJ75geLsB1Sfs",
-		},
-	} {
+  for _, test := range []struct {
+    In  io.Reader
+    Exp string
+  }{
+    {
+      In:  strings.NewReader(``),
+      Exp: "c459CSJESBh38BxDwwxNFKTXE4cC9HASGe3bhtN6z58GbwLqpCyRaKyZSvBAvTdF5NpSTPdUMH4hHRJ75geLsB1Sfs",
+    },
+  } {
 
-		actual := encode(test.In)
-		is.Equal(actual.String(), test.Exp)
+    actual := encode(test.In)
+    is.Equal(actual.String(), test.Exp)
 
-	}
+  }
 
 }
 
 func TestAllFFFF(t *testing.T) {
-	is := is.New(t)
-	var b []byte
-	for i := 0; i < 64; i++ {
-		b = append(b, 0xFF)
-	}
-	bignum := big.NewInt(0)
-	bignum = bignum.SetBytes(b)
-	id := c4.ID(*bignum)
-	is.Equal(id.String(), `c467RPWkcUr5dga8jgywjSup7CMoA9FNqkNjEFgAkEpF9vNktFnx77e2Js11EDL3BNu9MaKFUbacZRt1HYym4b8RNp`)
+  is := is.New(t)
+  var b []byte
+  for i := 0; i < 64; i++ {
+    b = append(b, 0xFF)
+  }
+  bignum := big.NewInt(0)
+  bignum = bignum.SetBytes(b)
+  id := c4.ID(*bignum)
+  is.Equal(id.String(), `c467RPWkcUr5dga8jgywjSup7CMoA9FNqkNjEFgAkEpF9vNktFnx77e2Js11EDL3BNu9MaKFUbacZRt1HYym4b8RNp`)
 
-	id2, err := c4.ParseID(`c467RPWkcUr5dga8jgywjSup7CMoA9FNqkNjEFgAkEpF9vNktFnx77e2Js11EDL3BNu9MaKFUbacZRt1HYym4b8RNp`)
-	is.NoErr(err)
-	bignum2 := big.Int(*id2)
-	b = (&bignum2).Bytes()
-	for _, bb := range b {
-		is.Equal(bb, 0xFF)
-	}
+  id2, err := c4.ParseID(`c467RPWkcUr5dga8jgywjSup7CMoA9FNqkNjEFgAkEpF9vNktFnx77e2Js11EDL3BNu9MaKFUbacZRt1HYym4b8RNp`)
+  is.NoErr(err)
+  bignum2 := big.Int(*id2)
+  b = (&bignum2).Bytes()
+  for _, bb := range b {
+    is.Equal(bb, 0xFF)
+  }
 }
 
 func TestAll0000(t *testing.T) {
-	is := is.New(t)
-	var b []byte
-	for i := 0; i < 64; i++ {
-		b = append(b, 0x00)
-	}
-	bignum := big.NewInt(0)
-	bignum = bignum.SetBytes(b)
-	id := c4.ID(*bignum)
-	is.Equal(id.String(), `c41111111111111111111111111111111111111111111111111111111111111111111111111111111111111111`)
+  is := is.New(t)
+  var b []byte
+  for i := 0; i < 64; i++ {
+    b = append(b, 0x00)
+  }
+  bignum := big.NewInt(0)
+  bignum = bignum.SetBytes(b)
+  id := c4.ID(*bignum)
+  is.Equal(id.String(), `c41111111111111111111111111111111111111111111111111111111111111111111111111111111111111111`)
 
-	id2, err := c4.ParseID(`c41111111111111111111111111111111111111111111111111111111111111111111111111111111111111111`)
-	is.NoErr(err)
-	bignum2 := big.Int(*id2)
-	b = (&bignum2).Bytes()
-	for _, bb := range b {
-		is.Equal(bb, 0x00)
-	}
+  id2, err := c4.ParseID(`c41111111111111111111111111111111111111111111111111111111111111111111111111111111111111111`)
+  is.NoErr(err)
+  bignum2 := big.Int(*id2)
+  b = (&bignum2).Bytes()
+  for _, bb := range b {
+    is.Equal(bb, 0x00)
+  }
 }
 
 func TestAppendOrder(t *testing.T) {
@@ -121,31 +120,33 @@ func TestAppendOrder(t *testing.T) {
 }
 
 func TestIDEncoder(t *testing.T) {
-	is := is.New(t)
-	e := c4.NewIDEncoder()
-	is.OK(e)
-	_, err := io.Copy(e, strings.NewReader(`This is a pretend asset file, for testing asset id generation.
+  is := is.New(t)
+  e := c4.NewIDEncoder()
+  is.OK(e)
+  _, err := io.Copy(e, strings.NewReader(`This is a pretend asset file, for testing asset id generation.
 `))
-	is.NoErr(err)
+  is.NoErr(err)
 
-	id := e.ID()
-	is.OK(id)
-	is.Equal(id.String(), `c43UBJqUTjQyrcRv43pgt1UWqysgNud7a7Kohjp1Z4w1gD8LGv4p1FK48kC8ufPPRpbEtc8inVhxuFQ453GcfRFE9d`)
+  id := e.ID()
+  is.OK(id)
+  is.Equal(id.String(), `c43UBJqUTjQyrcRv43pgt1UWqysgNud7a7Kohjp1Z4w1gD8LGv4p1FK48kC8ufPPRpbEtc8inVhxuFQ453GcfRFE9d`)
+  // Added test for mutability bug. Calling String() should not alter id!
+  is.Equal(id.String(), `c43UBJqUTjQyrcRv43pgt1UWqysgNud7a7Kohjp1Z4w1gD8LGv4p1FK48kC8ufPPRpbEtc8inVhxuFQ453GcfRFE9d`)
 }
 
 func TestParseBytesID(t *testing.T) {
-	is := is.New(t)
-	e := c4.NewIDEncoder()
-	is.OK(e)
-	_, err := io.Copy(e, strings.NewReader(`This is a pretend asset file, for testing asset id generation.
+  is := is.New(t)
+  e := c4.NewIDEncoder()
+  is.OK(e)
+  _, err := io.Copy(e, strings.NewReader(`This is a pretend asset file, for testing asset id generation.
 `))
-	is.NoErr(err)
+  is.NoErr(err)
 
-	id, err := c4.ParseBytesID([]byte(`c43UBJqUTjQyrcRv43pgt1UWqysgNud7a7Kohjp1Z4w1gD8LGv4p1FK48kC8ufPPRpbEtc8inVhxuFQ453GcfRFE9d`))
-	is.NoErr(err)
-	is.Equal(id, e.ID())
+  id, err := c4.ParseBytesID([]byte(`c43UBJqUTjQyrcRv43pgt1UWqysgNud7a7Kohjp1Z4w1gD8LGv4p1FK48kC8ufPPRpbEtc8inVhxuFQ453GcfRFE9d`))
+  is.NoErr(err)
+  is.Equal(id, e.ID())
 
-	id2, err := c4.ParseID(`c43UBJqUTjQyrcRv43pgt1UWqysgNud7a7Kohjp1Z4w1gD8LGv4p1FK48kC8ufPPRpbEtc8inVhxuFQ453GcfRFE9d`)
-	is.NoErr(err)
-	is.Equal(id2, e.ID())
+  id2, err := c4.ParseID(`c43UBJqUTjQyrcRv43pgt1UWqysgNud7a7Kohjp1Z4w1gD8LGv4p1FK48kC8ufPPRpbEtc8inVhxuFQ453GcfRFE9d`)
+  is.NoErr(err)
+  is.Equal(id2, e.ID())
 }
