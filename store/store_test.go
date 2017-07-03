5bf3d85af42194b1ff7484f1aa9cb5c4b8cc9472
Lars Lehtonen <lars.lehtonen@gmail.com>
2017-07-03 00:29:39 -0700

    Fix ignored error in TestWriter().

--- store/store_test.go
+++ store/store_test.go
@@ -208,6 +208,7 @@ func TestWriter(t *testing.T) {
 	w, err := st.Writer("/foo")
 	is.NoErr(err)
 	_, err = io.Copy(w, bytes.NewReader([]byte("bar")))
+	is.NoErr(err)
 	w.Close()
 	bar_id := c4.Identify(bytes.NewReader([]byte("bar")))
 	is.Equal(w.ID().String(), bar_id.String())
